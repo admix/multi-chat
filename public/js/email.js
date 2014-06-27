4920bbe3ad044c29ea3d3ade5a9ff62f3fd9c998
admix <admix.snurnikov@gmail.com>
2014-06-26 23:42:10 -0400

    Commented email sending

--- public/js/email.js
+++ public/js/email.js
@@ -28,16 +28,16 @@ function sendEmail(user) {
   }
 
   // send mail with defined transport object
-  smtpTransport.sendMail(mailOptions, function(error, response){
-      if(error){
-          console.log(error);
-      }else{
-          console.log("Message sent: " + response.message);
-      }
-
-      // if you don't want to use this transport object anymore, uncomment following line
-      //smtpTransport.close(); // shut down the connection pool, no more messages
-  });
+  // smtpTransport.sendMail(mailOptions, function(error, response){
+  //     if(error){
+  //         console.log(error);
+  //     }else{
+  //         console.log("Message sent: " + response.message);
+  //     }
+  //
+  //     // if you don't want to use this transport object anymore, uncomment following line
+  //     //smtpTransport.close(); // shut down the connection pool, no more messages
+  // });
 }
 
 exports.sendEmail = sendEmail;
